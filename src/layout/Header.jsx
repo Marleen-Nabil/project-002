8b0261d035450356b7374748eea00183f786d2e6
Marleen Nabil <eng.marleennabil@gmail.com>
2026-02-13 12:59:03 +0200

    import Header, Footer, Navbar, Body  to App.jsx

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,13 @@
+import Buttons from "../components/atoms/Buttons"
+
+
 export default function Header(){
     return(
-        <div>
-        
+        <div className='flex gap-2'>
+        <Buttons title='Home'/>
+        <Buttons className='Products'/>
+        <Buttons className='About Us'/>
+        <Buttons className='Index'/>
         </div>
     )
 }
