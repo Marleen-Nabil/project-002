9b7982104579c0d9548fddde34077c132f84110a
Marleen Nabil <eng.marleennabil@gmail.com>
2026-02-15 14:29:27 +0200

    edit buttons

--- src/components/atoms/Buttons.jsx
+++ src/components/atoms/Buttons.jsx
@@ -1,5 +1,10 @@
-export default function Buttons(){
+export default function Buttons(props){
+    
+    
+    
     return(
-        <div></div>
+        <div>
+            <button className={`m-2 border-3 border-amber-300 border-solid rounded-xl p-2 font-medium`}>{props.title}</button>
+        </div>
     )
 }
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,12 +2,17 @@ import Buttons from "../components/atoms/Buttons"
 
 
 export default function Header(){
+
+
+
+
+    
     return(
         <div className='flex gap-2'>
         <Buttons title='Home'/>
-        <Buttons className='Products'/>
-        <Buttons className='About Us'/>
-        <Buttons className='Index'/>
+        <Buttons title='Products'/>
+        <Buttons title='About Us'/>
+        <Buttons title='Index'/>
         </div>
     )
 }
