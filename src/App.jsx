44d36c8c2a4eeddb36a80307213657a24ee406b1
Marleen Nabil <eng.marleennabil@gmail.com>
2026-02-22 16:16:54 +0200

    fix main and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './layout/Header'
 import Footer from './layout/Footer'
@@ -18,4 +16,4 @@ function App() {
   )
 }
 
-export default App
+
